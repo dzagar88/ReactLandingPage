8943cc8c3b5464b93519e6d5c00b0d68a3e1f8ce
dzagar88 <dzagarit@gmail.com>
2023-03-16 21:23:20 +0000

    updated bottom bar and col heights

---
+++ src/js/component/Bottombar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Bottombar = () => {
+    return (
+        <div className="bottom-bar">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand mx-auto" href="#">Copyright © Our Website 2023</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Bottombar;
